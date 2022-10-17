5512245eff502847412a65b426b1ff17c7761fe1
hanbee17 <hanbee.cho@sjsu.edu>
2022-10-17 02:03:08 -0700

    1

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 let gradient;
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(400, 400);
   // create a linear gradient that's angled at 0 radians, and is 200px wide
   gradient = createLinearGradient(0, 200);
   // add some colors
   // at 0% make it lightblue, then at 50% make it pink, and at 100% make it magenta
-  gradient.colors(0, "lightblue", 0.5, "pink", 1);
+  gradient.colors(0, "lightblue", 0.5, "pink", 1, "magenta");
 }
 
 function draw() {
