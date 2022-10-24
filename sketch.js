f0360b4d0e42129e0e8a23fa83b35cc93fc3e9c2
hanbee17 <hanbee.cho@sjsu.edu>
2022-10-24 14:23:03 -0700

    1

--- sketch.js
+++ sketch.js
@@ -1,40 +1,54 @@
-//let initialButton;
+let initialButton;
 let pageState = 'first';
 
 function setup() {
   cvn = createCanvas(windowWidth, windowHeight * 0.8);
+  cvn.parent('canvasPage');
+
+  background(0);
+  intialButtons();
+  firstScreen();
 }
 
-// function startButton() {
-// initialButton.locate(windowWidth * 0.445, windowHeight * 0.35);
-// }
+function startButton() {
+initialButton.locate(windowWidth * 0.445, windowHeight * 0.35);
+}
 
 function draw() {
-
   switch (pageState) {
     case 'title':
       firstScreen();
       break;
+    case 'main':
+      questionPage();
+      break;
     }
 
-  //initialButton.draw();
+    initialButton.draw();
+
 }
 
-// function intialButtons() {
-//   initialButton = new Clickable();
-//   initialButton.locate(-400, -500);
-//   initialButton.resize(windowWidth * 1, windowWidth * 1);
-//   initialButton.text = "         ";
-//   initialButton.color = "#00000000";
-//   initialButton.strokeWeight = 00;
-//   initialButton.onRelease = function() {
-//     pageState = 'main'
-//     initialButton.locate(-400, -500);
-//   }
-// }
+function intialButtons() {
+  initialButton = new Clickable();
+  initialButton.locate(-400, -500);
+  initialButton.resize(windowWidth * 1, windowWidth * 1);
+  initialButton.color = "#00000000";
+  initialButton.textColor = "#000000";
+  initialButton.strokeWeight = 00;
+  initialButton.text = "Click To Start";
+  initialButton.onRelease = function() {
+    pageState = 'main'
+    initialButton.locate(-400, -500);
+  }
+}
 
 function firstScreen() {
-  createCanvas(windowWidth * 1, windowHeight * 2.6);
-  initialButton();
+  createCanvas(windowWidth * 1, windowHeight * 1);
+  background(220);
+  startButton();
+}
 
+function questionPage() {
+  createCanvas(windowWidth * 1, windowHeight * 2.6);
+  background(0);
 }
